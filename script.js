4c124fb64368abf8ad3be3f4f1898a89ab37912d
devin-lynch <devin.l.lynch@gmail.com>
2022-08-18 01:07:10 -0500

    Create function to run game

--- script.js
+++ script.js
@@ -9,16 +9,22 @@ const paper = document.querySelector('#paper')
 const scissors = document.querySelector('#scissors')
 const choices = ["Rock", "Paper", "Scissors"]
 
-rock.addEventListener('click', () => {
-   return playerChoice = choices[0]
+rock.addEventListener('click', function() {
+    playerChoice = choices[0]
+    console.log(playerChoice)
+    runGame()
 })
 
-paper.addEventListener('click', () => {
-    return playerChoice = choices[1]
+paper.addEventListener('click', function() {
+    playerChoice = choices[1]
+    console.log(playerChoice)
+    runGame()
 })
 
-scissors.addEventListener('click', () => {
-    return playerChoice = choices[2]
+scissors.addEventListener('click', function() {
+    playerChoice = choices[2]
+    console.log(playerChoice)
+    runGame()
 })
 
 
@@ -29,42 +35,45 @@ computerChoice = choices[randomizer]
 
 console.log(playerChoice)
 console.log(computerChoice)
+function runGame() {
+    computerChoice = choices[randomizer]
+    if (computerChoice === choices[0]) {
+        if (playerChoice === choices[1]){
+            console.log(`You win! You selected ${playerChoice} and the computer selected ${choices[0]}`)
+            result.innerText = `You win! You selected ${playerChoice} and the computer selected ${choices[0]}`
+        } else if (playerChoice === choices[2]) {
+            console.log(`You lose! You selected ${playerChoice} and the computer selected ${choices[0]}`)
+            result.innerText = `You lose! You selected ${playerChoice} and the computer selected ${choices[0]}`
+        } else {
+            console.log(`An even display of skill! You both selected ${playerChoice}`)
+            result.innerText = `An even display of skill! You both selected ${playerChoice}`
+        }
+    }
 
-if (computerChoice === choices[0]) {
-    if (playerChoice === choices[1]){
-        console.log(`You win! You selected ${playerChoice} and the computer selected ${choices[0]}`)
-        result.innerText = `You win! You selected ${playerChoice} and the computer selected ${choices[0]}`
-    } else if (playerChoice === choices[2]) {
-        console.log(`You lose! You selected ${playerChoice} and the computer selected ${choices[0]}`)
-        result.innerText = `You lose! You selected ${playerChoice} and the computer selected ${choices[0]}`
-    } else {
-        console.log(`An even display of skill! You both selected ${playerChoice}`)
-        result.innerText = `An even display of skill! You both selected ${playerChoice}`
+    if (computerChoice === choices[1]) {
+        if (playerChoice === choices[2]){
+            console.log(`You win! You selected ${playerChoice} and the computer selected ${choices[1]}`)
+            result.innerText = `You win! You selected ${playerChoice} and the computer selected ${choices[1]}`
+        } else if (playerChoice === choices[0]) {
+            console.log(`You lose! You selected ${playerChoice} and the computer selected ${choices[1]}`)
+            result.innerText = `You lose! You selected ${playerChoice} and the computer selected ${choices[1]}`
+        } else {
+            console.log(`An even display of skill! You both selected ${playerChoice}`)
+            result.innerText = `An even display of skill! You both selected ${playerChoice}`
+        }
+    }
+
+    if (computerChoice === choices[2]) {
+        if (playerChoice === choices [0]) {
+            console.log(`You win! You selected ${playerChoice} and the computer selected ${choices[2]}`)
+            result.innerText = `You win! You selected ${playerChoice} and the computer selected ${choices[2]}`
+        } else if (playerChoice === choices[1]) {
+            console.log(`You lose! You selected ${playerChoice} and the computer selected ${choices[2]}`)
+            result.innerText = `You lose! You selected ${playerChoice} and the computer selected ${choices[2]}`
+        } else {
+            console.log(`An even display of skill! You both selected ${playerChoice}`)
+            result.innerText = `An even display of skill! You both selected ${playerChoice}`
+        }
     }
 }
 
-if (computerChoice === choices[1]) {
-    if (playerChoice === choices[2]){
-        console.log(`You win! You selected ${playerChoice} and the computer selected ${choices[1]}`)
-        result.innerText = `You win! You selected ${playerChoice} and the computer selected ${choices[1]}`
-    } else if (playerChoice === choices[0]) {
-        console.log(`You lose! You selected ${playerChoice} and the computer selected ${choices[1]}`)
-        result.innerText = `You lose! You selected ${playerChoice} and the computer selected ${choices[1]}`
-    } else {
-        console.log(`An even display of skill! You both selected ${playerChoice}`)
-        result.innerText = `An even display of skill! You both selected ${playerChoice}`
-    }
-}
-
-if (computerChoice === choices[2]) {
-    if (playerChoice === choices [0]) {
-        console.log(`You win! You selected ${playerChoice} and the computer selected ${choices[2]}`)
-        result.innerText = `You win! You selected ${playerChoice} and the computer selected ${choices[2]}`
-    } else if (playerChoice === choices[1]) {
-        console.log(`You lose! You selected ${playerChoice} and the computer selected ${choices[2]}`)
-        result.innerText = `You lose! You selected ${playerChoice} and the computer selected ${choices[2]}`
-    } else {
-        console.log(`An even display of skill! You both selected ${playerChoice}`)
-        result.innerText = `An even display of skill! You both selected ${playerChoice}`
-    }
-}
