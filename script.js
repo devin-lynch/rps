c3fe32f3b2056b8aa130ef02407b0c8a96483183
devin-lynch <devin.l.lynch@gmail.com>
2022-08-18 00:38:21 -0500

    Add boilerplate and win conditions

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log('hola from js!')
+
+let playerChoice
+let computerChoice
+const randomizer = Math.floor(Math.random() * 3)
+
+
+const choices = ["Rock", "Paper", "Scissors"]
+playerChoice = choices[2]
+computerChoice = choices[randomizer]
+
+console.log(playerChoice)
+console.log(computerChoice)
+
+if (computerChoice === choices[0]) {
+    if (playerChoice === choices[1]){
+        console.log(`You win! You selected ${playerChoice} and the computer selected ${choices[0]}`)
+    } else if (playerChoice === choices[2]) {
+        console.log(`You lose! You selected ${playerChoice} and the computer selected ${choices[0]}`)
+    } else {
+        console.log(`An even display of skill! You both selected ${playerChoice}`)
+    }
+}
+
+if (computerChoice === choices[1]) {
+    if (playerChoice === choices[2]){
+        console.log(`You win! You selected ${playerChoice} and the computer selected ${choices[1]}`)
+    } else if (playerChoice === choices[0]) {
+        console.log(`You lose! You selected ${playerChoice} and the computer selected ${choices[1]}`)
+    } else {
+        console.log(`An even display of skill! You both selected ${playerChoice}`)
+    }
+}
+
+if (computerChoice === choices[2]) {
+    if (playerChoice === choices [0]) {
+        console.log(`You win! You selected ${playerChoice} and the computer selected ${choices[2]}`)
+    } else if (playerChoice === choices[1]) {
+        console.log(`You lose! You selected ${playerChoice} and the computer selected ${choices[2]}`)
+    } else {
+        console.log(`An even display of skill! You both selected ${playerChoice}`)
+    }
+}
